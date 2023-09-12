738f3d1d3e194238e84423648c6404a960825792
bammint <baikmalss@naver.com>
2023-09-12 17:59:58 +0900

    0912

---
+++ 72_slide/슬라이드_data.js
@@ -0,0 +1,23 @@
+// 데이터 정의
+const imgObj = {
+    imgData1: {
+        imgSrc: '/assets/images/cat1.jpg',
+        imgAlt: '고양이1'
+    },
+    imgData2: {
+        imgSrc: '/assets/images/cat2.jpg',
+        imgAlt: '고양이2'
+    },
+    imgData3: {
+        imgSrc: '/assets/images/cat3.jpg',
+        imgAlt: '고양이3'
+    },
+    imgData4: {
+        imgSrc: '/assets/images/cat4.jpg',
+        imgAlt: '고양이4'
+    },
+    imgData5: {
+        imgSrc: '/assets/images/cat5.jpg',
+        imgAlt: '고양이5'
+    }
+}
