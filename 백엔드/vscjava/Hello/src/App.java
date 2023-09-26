75f978c20f88d04bb0207ad34d4667593738a6ec
bammint <baikmalss@naver.com>
2023-09-26 23:22:12 +0900

    0926

--- 백엔드/vscjava/Hello/src/App.java
+++ 백엔드/vscjava/Hello/src/App.java
@@ -1,14 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        int intValue = 10;
-        char charValue = 'A';
-        double doubleValue = 5.7;
-        String strValue = "A";
-
-        
-        if(charValue == strValue){
-            System.out.println();
+        int v1 = 1;
+        System.out.println("v1: " + v1);
+        if (true) {
+            int v2 = 2;
+            if (true) {
+                int v3 = 2;
+                System.out.println("v1: " + v1);
+                System.out.println("v2: " + v2);
+                System.out.println("v3: " + v3);
+            }
+            System.out.println("v1: " + v1);
+            System.out.println("v2: " + v2);
+            System.out.println("v3: " + v3);
         }
+        System.out.println("v1: " + v1);
+        System.out.println("v2: " + v2);
     }
 }
