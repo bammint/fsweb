3be84f55252350d4388ac4516e09fd36828596dc
bammint <baikmalss@naver.com>
2023-09-26 17:51:19 +0900

    0926

--- 백엔드/vscjava/Hello/src/App.java
+++ 백엔드/vscjava/Hello/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int intValue = 10;
+        char charValue = 'A';
+        double doubleValue = 5.7;
+        String strValue = "A";
+
+        
+        if(charValue == strValue){
+            System.out.println();
+        }
     }
 }
