7206ed1bb41e9101f055b81c99f67df9bb713ba6
bammint <baikmalss@naver.com>
2023-10-07 02:23:41 +0900

    1007

--- 백엔드/vscjava/Hello/src/exam/ex44.java
+++ 백엔드/vscjava/Hello/src/exam/ex44.java
@@ -8,7 +8,7 @@ public class ex44 {
         System.out.println("요일을 입력해주세요");
         int num = scan.nextInt();
 
-        String[] str = {"월요일","화요일","수요일","목요일","금요일","토요일","일요일",};
+        String[] str = {"월요일","화요일","수요일","목요일","금요일","토요일","일요일"};
 
         int i = num-1;
             if(num < 8){
