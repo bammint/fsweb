96bb3a0b3db3bbe24a391643d911790597e78125
bammint <baikmalss@naver.com>
2023-10-12 01:18:34 +0900

    1012

--- 백엔드/vscjava/Hello/src/exam/ex59.java
+++ 백엔드/vscjava/Hello/src/exam/ex59.java
@@ -19,6 +19,7 @@ public class ex59 {
         // for(int i=0; i<lottoNum.length; i++ ){
         // System.out.println(lottoNum[i]);
         // }
+        System.out.println("나의 번호");
         int myNum1 = scan.nextInt();
         int myNum2 = scan.nextInt();
         int myNum3 = scan.nextInt();
@@ -26,10 +27,28 @@ public class ex59 {
         int myNum5 = scan.nextInt();
         int myNum6 = scan.nextInt();
         int[] myNum = { myNum1, myNum2, myNum3, myNum4, myNum5, myNum6 };
+        int[] n = new int[6];
 
-        for(int i=0; i<lottoNum.length; i++ ){
-            if((lottoNum[i]==myNum[i])){
-
+        for (int i = 0; i < lottoNum.length; i++) {
+            for (int j = 0; j < myNum.length; j++) {
+                if (lottoNum[i] == myNum[j]) {
+                    // System.out.println(myNum[j]);
+                    if (myNum[j] == 3 && myNum[j] <4) {
+                        System.out.println("5 당첨");
+                    }
+                    if (myNum[j] == 4 && myNum[j] <5) {
+                        System.out.println("4 당첨");
+                    }
+                    if (myNum[j] == 5 && myNum[j] <6) {
+                        System.out.println("3 당첨");
+                        if (myNum[j] == bonusNum) {
+                            System.out.println("2 당첨");
+                        }
+                    }
+                    if (myNum[j] == 6) {
+                        System.out.println("1 당첨");
+                    }
+                }
             }
         }
     }
---
+++ 백엔드/vscjava/Hello/src/exam/ex59_1.java
@@ -0,0 +1,13 @@
+package exam;
+
+import java.util.Scanner;
+
+public class ex59_1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String lottoNum = scan.next();
+        String myNum = scan.next();
+
+        
+    }
+}
