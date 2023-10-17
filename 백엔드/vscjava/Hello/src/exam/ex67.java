fbd911424ac2b86b6a3e0c546b9a12e5f59b15c2
bammint <baikmalss@naver.com>
2023-10-17 15:06:27 +0900

    1017

---
+++ 백엔드/vscjava/Hello/src/exam/ex67.java
@@ -0,0 +1,32 @@
+package exam;
+
+import java.util.Scanner;
+
+public class ex67 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+
+        // for (int i = a; i > 0; i--) {
+        //     if (i == 0) {
+        //         break;
+        //     }
+        //     System.out.println(i);
+        // }
+
+        System.out.print("카운트 다운 시작할 숫자를 입력하세요: ");
+        int i = a;
+        while (i > 0) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println(i);
+            i--;
+        }
+
+        System.out.println("종료합니다.");
+    }
+}
