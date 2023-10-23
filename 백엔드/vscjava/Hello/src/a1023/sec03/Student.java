9aa5b904192f63b528dd7d9553bb0483bc78165d
bammint <baikmalss@naver.com>
2023-10-23 14:15:49 +0900

    1023

---
+++ 백엔드/vscjava/Hello/src/a1023/sec03/Student.java
@@ -0,0 +1,14 @@
+package a1023.sec03;
+
+public class Student {
+    private String name;
+    private int score;
+
+    public Student (String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getaName(){return name;}
+    public int getScore(){return score;}
+}
