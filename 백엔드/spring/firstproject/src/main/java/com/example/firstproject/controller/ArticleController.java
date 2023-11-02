a7e157f881525bbc9e0ed6d6ed3ecfbd7393eb11
bammint <baikmalss@naver.com>
2023-11-02 16:35:31 +0900

    1102

--- 백엔드/spring/firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ 백엔드/spring/firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -82,4 +82,33 @@ public class ArticleController {
         // 보여줄 페이지를 설정
         return "articles/show";
     }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id,Model model){
+        // 수정할 데이터 가져오기 articleEntity
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 모델 데이터 등록
+        model.addAttribute("article",articleEntity);
+        // 뷰페이지 설정
+        return "articles/edit";
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form){
+        log.info(form.toString());
+        // 1. Dto를 엔티티로 변환
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+
+        // 2. 엔티티를 DB로 저장
+        //2-1. DB에서 기존 데이터를 가져옴
+        Article target = articleRepository.findById(articleEntity.getId())
+                    .orElse(null);
+        // 2-2. 기존의 데이터가 있다면 값을 갱신
+        if(target != null){
+            articleRepository.save(articleEntity);
+        }
+        // 수정 결과 페이지로 리다이렉트
+        return "redirect:/articles/" + articleEntity.getId();
+    }
 }
