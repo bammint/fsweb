c25b8e66e32e754756969fc4b5c586c07d91bd1b
bammint <baikmalss@naver.com>
2023-11-07 00:17:39 +0900

    1107

---
+++ 백엔드/spring/board/src/main/java/board/service/BoardService.java
@@ -0,0 +1,20 @@
+package board.service;
+
+import board.dto.BoardDTO;
+import board.entity.BoardEntity;
+import board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+// DTO -> Entity (Entity Class)
+// Entity -> DTO (DTO Class)
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final BoardRepository boardRepository;
+    public void save(BoardDTO boardDTO) {
+        BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDTO);
+        boardRepository.save(boardEntity);
+    }
+}
