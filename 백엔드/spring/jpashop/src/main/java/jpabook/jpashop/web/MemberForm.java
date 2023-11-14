acf7d83b7aa83ce62bac113c92bb5a2c041131dd
bammint <baikmalss@naver.com>
2023-11-14 16:04:26 +0900

    1114

--- 백엔드/spring/jpashop/src/main/java/jpabook/jpashop/web/MemberForm.java
+++ 백엔드/spring/jpashop/src/main/java/jpabook/jpashop/web/MemberForm.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop.web;
 
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +15,15 @@ public class MemberForm {
     private String city;
     private String street;
     private String zipcode;
+
+//    public static Member createMember(MemberForm form){
+//        Address address = new Address(form.getCity(), form.getStreet(),form.getZipcode());
+//
+//        Member member = new Member();
+//        member.setName(form.getName());
+//        member.setAddress(address);
+//        return member;
+//    }
 }
 // @NotEmpty
 // null 안되고 " " 빈문자열 안됨 공백문자도 안됨
