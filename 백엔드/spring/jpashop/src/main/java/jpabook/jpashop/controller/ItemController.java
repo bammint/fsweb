720dbcf060ee1f117b1e8fadbeaa0ee7ba11849a
bammint <baikmalss@naver.com>
2023-11-15 13:02:35 +0900

    1115

--- 백엔드/spring/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ 백엔드/spring/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -1,13 +1,19 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.web.BookForm;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -32,4 +38,55 @@ public class ItemController {
         itemService.saveItem(book);
         return "redirect:/items";
     }
+
+    @GetMapping("/items")
+    public String list(Model model){
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items",items);
+        return "items/itemList";
+    }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model){
+        Book item = (Book)itemService.findOne(itemId);
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    public String updateItem(@PathVariable Long itemId,@ModelAttribute("form") BookForm form){
+//        Book book = new Book();
+//        book.setId(form.getId());
+//        book.setName(form.getName());
+//        book.setPrice(form.getPrice());
+//        book.setStockQuantity(form.getStockQuantity());
+//        book.setAuthor(form.getAuthor());
+//        book.setIsbn(form.getIsbn());
+//        itemService.saveItem(book);
+        itemService.updateItem(itemId, form.getName(),form.getPrice(),form.getStockQuantity());
+        return "redirect:/items";
+    }
 }
+// 병합(merge)
+// 영속성 엔티티가 더이상 관리하지 않는 엔티티이며 이미 한번
+// parsist되어서 식별자가 있는 경우를 뜻한다
+// 값이 변경되어도 알아서 반영이 되지 않기 때문에 따로 값을 반영해야한다
+
+// 파라미터 item 객체는 준영속 엔티티이며 em,merge를 통해
+// Item merge는 파라미터로 받은 item으로 값들이 다 교체된다
+
+// merge 할 경우 강제적으로 모든 필드의 값을 교체하고 값이 없으면
+// null로 돌아가기 때문에 조심해야한다
+
+// merge 할 경우 강제적으로 모든 필드의 값들이 변경되기 때문에
+// "엔티티를 변경할 때는 항상 변경감지를 사용한다"
+
+// @Controller에서 식별자(primary key)만 넘기고 핵심 비즈니스
+// 로직은 @Transactional(@Service)안에서 처리하는 것이 좋다
