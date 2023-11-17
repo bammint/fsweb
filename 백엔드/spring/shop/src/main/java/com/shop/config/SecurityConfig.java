b2fdfde27eafab01840705b7c14c7442557291ff
bammint <baikmalss@naver.com>
2023-11-17 13:57:55 +0900

    1117

---
+++ 백엔드/spring/shop/src/main/java/com/shop/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.shop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin();
+
+        http.authorizeRequests()
+                .mvcMatchers("/css/**","/js/**","/img/**").permitAll()
+                .mvcMatchers("/","/members/**","/item/**","/images/**").permitAll()
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated();
+        // static 밑에 css, js, img 폴더는 접근 권한을 줌
+        // "/","/members/**","/item/**","/images/**" 모든 사용자에게 접근 허용
+
+        // http.authorizeRequests() 권한부여
+        // anyRequest() 모든 요청에 권한 설정()
+        // authenticated() 인증된 사용자만이 해당 요청에 접근
+
+        return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
