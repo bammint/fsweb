bbd76da63491c497b9d11423d16439c66b56e90a
bammint <baikmalss@naver.com>
2023-11-21 17:36:39 +0900

    1121

---
+++ Back/spring/shop/src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,48 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.dto.ItemFormDto;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item")
+@Getter
+@Setter
+@ToString
+public class Item extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "item_id")
+    private Long id; // 상품코드
+
+    @Column(nullable = false, length = 50)
+    private String itemNm; // 상품명
+    // null을 허용하지 않고 itemNm필드는 항상 값을 가져야한다
+    // 최대 50자
+
+    @Column(name = "price", nullable = false)
+    private int price; // 가격
+
+    @Column(nullable = false)
+    private int stockNumber; // 재고 수량
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail; // 상품 상세 설명
+    // @Lob - 대용량의 데이터를 저장
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus; // 상품 판매 상태
+
+    public void updateItem(ItemFormDto itemFormDto) {
+        this.itemNm = itemFormDto.getItemNm();
+        this.price = itemFormDto.getPrice();
+        this.stockNumber = itemFormDto.getStockNumber();
+        this.itemDetail = itemFormDto.getItemDetail();
+        this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
+}
