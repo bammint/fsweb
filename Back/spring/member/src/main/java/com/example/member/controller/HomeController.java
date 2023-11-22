13b86b8c9911f1ee125a08c3802bbb3bb53b2f4e
bammint <baikmalss@naver.com>
2023-11-23 00:07:30 +0900

    1123

--- Back/spring/member/src/main/java/com/example/member/controller/HomeController.java
+++ Back/spring/member/src/main/java/com/example/member/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
     // 기본 페이지 요청 메서드
     @GetMapping("/")
     public String index(){
-        return "index"; // => templates 폴더의 index.html을 찾아감
+        return "index";
     }
 }
---
+++ Back/spring/member/src/main/java/com/example/member/entity/Member.java
@@ -0,0 +1,8 @@
+package com.example.member.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Member {
+
+}
